47eb62aa1188c2f4d0edd8ae3c8bbd61f8938187
Farah Student <farah.gus@hotmail.com>
2019-03-01 13:16:24 -0500

    display results function

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
 'use strict';
 /* global $ */
 
-const STORE = {
-  repos: [],
-};
-
-function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random/3')
+function getDogImage(defaultNum) {
+  fetch(`https://dog.ceo/api/breeds/image/random/${defaultNum}`)
     .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
+    .then(responseJson => displayResults(responseJson));
 }
+
+/*/USER INPUT # OF IMAGES
+function numberOfImages(num){
+  console.log(`Retrieving ${num} dog pictures.`);
+  STORE.push(num, getDogImage());
+} */
+
+function displayResults(responseJson){
+  console.log(responseJson);
+  $('.results-img').replaceWith(
+    `<img src="${responseJson.message}" class="results-img">`
+  );
+  //display the results section
+  $('.results').removeClass('hidden');
+} 
 
 // EVENT LISTENER FOR FORM
 function handleForm(){
   $('form').submit(event => {
     console.log('button ran');
+    let defaultNum = 3;
+    let userInput = $('.js-dog-images-entry').val();
+    if (typeof userInput === 'number') {
+      defaultNum = userInput;
+    }
+    $('.js-dog-images-entry').val('');
     event.preventDefault();
-    getDogImage();
+    getDogImage(userInput);
   });
 }
 
-function main() {
+
+$(function() {
   console.log('App loaded! Waiting for submit!');
   handleForm();
-}
-$(main);
+  
+});
