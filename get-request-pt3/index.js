d2e2fdb4978a041a7431f1debb846b2fa2b4d400
Farah Student <farah.gus@hotmail.com>
2019-03-03 15:39:23 -0500

    fixed alert error

--- get-request-pt3/index.js
+++ get-request-pt3/index.js
@@ -1,50 +1,37 @@
 'use strict';
-/* global $ */
+/*global $*/
 
 function getDogImage(breedInput) {
   fetch(`https://dog.ceo/api/breed/${breedInput}/images/random`)
     .then(response => response.json())
     .then(responseJson => displayResults(responseJson))
-    .catch(error => alert('Something went wrong'));
+    .catch(error => alert('Something went wrong.'));
 }
 
 function displayResults(responseJson){
   console.log(responseJson); 
-  if (responseJson.status === 'error'){
-    //alert('Cannot find dog breed.');
-    const breedNotFound ='<p><b>Whoops! We can\'t find that breed.</b></p>';
-    $('.results').html(breedNotFound);
+  if (responseJson.status === 'error') {
+    alert('Can\'t find dog breed.');
   }
   else {
     const randomDogBreed = `<img src="${responseJson.message}" class="results-img">`;
     $('.results').html(randomDogBreed);
   }
-
-  
 }
-
-  
-
 
 // EVENT LISTENER FOR FORM
 function handleForm(){
   $('form').submit(event => {
+    event.preventDefault();
     console.log('button ran');
     let userInput = $('.js-dog-images-entry').val();
-    /*let defaultNum = 3;
-    
-    if (typeof userInput === 'number') {
-      defaultNum = userInput;
-    } */
     $('.js-dog-images-entry').val('');
-    event.preventDefault();
+    
     getDogImage(userInput);
   });
 }
 
-
 $(function() {
   console.log('App loaded! Waiting for submit!');
   handleForm();
-  
 });
